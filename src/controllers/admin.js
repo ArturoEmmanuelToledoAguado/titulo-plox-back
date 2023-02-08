e189afedbd8a41118894c4de22c0ac1667c34edf
ThunderGer <jaames11@hotmail.com>
2023-02-08 17:20:03 -0600

    someone changes

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,25 +1,31 @@
 const source = (req, res) => {
     res.send(`Here recent events of system`);
+    // res.json({});
 }
 
 const home = (req, res) => {
     res.send(`Here are home`);
+    // res.json({});
 }
 
 const users = (req, res) => {
     res.send(`Here are users of system`);
+    // res.json({});
 }
 
 const deleteUsers = (req, res) => {
     req.send(`Here delete users of system`);
+    // res.json({});
 }
 
 const docs = (req, res) => {
     res.send(`Here are docs of users`);
+    // res.json({});
 }
 
 const deleteDocuments = (req, res) => {
     res.send(`Here delete documents of users`);
+    // res.json({});
 }
 
 // This last route is merely optional and
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,27 +1,33 @@
 const sendFile = (req, res) => {
     res.send(`Here are the files`);
+    // res.json({});
 }
 
 const viewFiles = (req, res) => {
     res.send(`Here are all the files`);
+    // res.json({});
 }
 
 const sendReport = (req, res) => {
     res.send(`Here are the reports`);
+    // res.json({});
 }
 
 // ! Do not implement if it is not needed because
 // ! this route is last merely fury development
 const viewReports = (req, res) => {
     res.send(`Here are all the reports of files`);
+    // res.json({});
 }
 
 const deleteFile = (req, res) => {
     res.send(`Here are detele the files`);
+    // res.json({});
 }
 
 const profile = (req, res) => {
     res.send(`Here are the config and profile preferences`);
+    // res.json({});
 }
 
 module.exports = {
